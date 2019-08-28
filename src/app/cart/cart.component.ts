9d416c58e8ad2e7ef5425154051378216747bdf3
Your Name <you@example.com>
2019-08-28 15:23:32 +0700

    bài tập web

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,8 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
- 
+  pageTitle="danh sách sản phẩm";
+  isShow=true;
+  listproduct=[
+    {
+      "productId": 1,
+      "LastName": "Tuấn",
+      "FirstName": "Mạc",
+      "EMail": "tuan@gmail.com",
+      "Action": "Action",
+    },
+    {
+      "productId": 2,
+      "LastName": "Vũ",
+      "FirstName": "Tạ",
+      "EMail": "vu@gmail.com",
+      "Action": "Action",
+    },
+    {
+      "productId": 3,
+      "LastName": "Phương",
+      "FirstName": "Võ",
+      "EMail": "phuong@gmail.com",
+      "Action": "Action",
+    },
+    {
+      "productId": 4,
+      "LastName": "Duy",
+      "FirstName": "GMG-0042",
+      "EMail": "Duy@gmail.com",
+      "Action": "Action",
+    },
+    {
+        "productId": 5,
+        "LastName": "Thái",
+        "FirstName": "Nguyễn",
+        "EMail": "Thai@gmail.com",
+        "Action": "Action",
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
