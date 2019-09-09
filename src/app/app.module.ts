b4dba2aade8ad5ef2c74110b90fb58b32a50c1ea
Your Name <you@example.com>
2019-09-09 16:28:02 +0700

    bai tap web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
@@ -16,7 +17,14 @@ import { Cart1Component } from './cart1/cart1.component';
     Cart1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '',component: ProductComponent },
+      {path: 'product', component:ProductComponent},
+      {path: 'cart1', component:Cart1Component},
+      { path: 'product/:productId', component: ProductdetaillComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full' },
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/productdetaill/productdetaill.component.ts
+++ src/app/productdetaill/productdetaill.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-productdetaill',
   templateUrl: './productdetaill.component.html',
   styleUrls: ['./productdetaill.component.css']
 })
 export class ProductdetaillComponent implements OnInit {
-
-  constructor() { }
+product:any;
+pageTitle='';
+pid;
+listproduct=[
+  {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "choi.jpg"
+  },
+  {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "xe-gom-rac-ton.gif"
+  },
+  {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "buasat.jpg"
+  },
+  {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "cuago.jpg"
+  },
+  {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "dochoidientu.jpg"
+  },
+];
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(para=>{
+      this.pid=+para.get('productId');
+    });
+    this.product=this.listproduct.find(p=>p.productId===this.pid);
   }
 
 }
